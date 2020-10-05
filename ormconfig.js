7e0a13283a36a637cd10d7ac03254d745025ad90
igorcruzz <igorskt2009@gmail.com>
2020-10-05 11:15:45 -0300

    test

--- ormconfig.js
+++ ormconfig.js
@@ -2,11 +2,11 @@ require('dotenv').config
 
 module.exports = {
   type: 'postgres',    
-  host: 'localhost',
+  host: process.env.DB_HOST,
   port: 5432, 
-  username: 'companyhub',
-  password: 'narutoplayers',
-  database: 'companyhub',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migration/**/*.js'],
   cli: {
