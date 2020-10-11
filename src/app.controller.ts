31e39fd75eae85203d0b27a1774638f233b7bcc2
igorcruzz <igorskt2009@gmail.com>
2020-10-11 16:01:05 -0300

    app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common'
 @Controller('/')
 export class AppController {
   @Get()
-  async execute(): Promise<any> {
+  execute(): any {
     return { app: 'Companyhub' }
   }
 }
